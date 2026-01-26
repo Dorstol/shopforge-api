5d6c86bf90666c702a8b915cd23534bb25ffbd1a
Dorstol <olehsofronov@gmail.com>
2026-01-26 12:00:07 +0200

    categories model

---
+++ backend/app/apps/products/models.py
@@ -0,0 +1,17 @@
+import datetime as dt
+from apps.core.base import Base
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False,)
+    version: Mapped[str] = mapped_column(Integer, nullable=False, default=1,)
+    updated_at: Mapped[dt.datetime] = mapped_column(default=func.now(), onupdate=func.now(),)
+
+    def __str__(self) -> str:
+        return f"<Category {self.name} - #{self.id}>"
